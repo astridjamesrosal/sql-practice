70f6d1f3d8ce416a802ae036f2c88674d151a0bf
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-05-26 13:29:28 +0800

    topic3 - Add INSERT CRUD operations for Books Table

---
+++ queries/topic3_schema/library_crud.sql
@@ -0,0 +1,13 @@
+-- INSERT
+-- Insert "The Night We Met" by Abby Jimenez with its year with the status of available.
+INSERT INTO Books (title, author, year, status)
+VALUES ('The Night We Met', 'Abby Jimenez', 2026, 'available');
+
+-- Insert "Yesteryear" by Caro Burke with its year with the status of borrowed.
+INSERT INTO Books (title, author, year, status)
+VALUES ('Yesteryear', 'Caro Claire Burke', 2026, 'borrowed');
+
+-- Insert "Our Perfect Storm" by Carley Fortune with its year with the status of available.
+INSERT INTO Books (title, author, year, status)
+VALUES ('Our Perfect Storm', 'Carley Fortune', 2026, 'available');
+
